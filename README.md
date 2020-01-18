39f1d4d91c54b1f7af1c3c7dfc0586f809cf2d85
sruthiragupathy <37189469+sruthiragupathy@users.noreply.github.com>
2020-01-18 17:54:49 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react_practice
-Created with CodeSandbox
---
+++ src/components/MyInfo.js
@@ -0,0 +1,22 @@
+import React from "react";
+function MyInfo() {
+  return (
+    <div>
+      <p>
+        The last holiday i went for is tirupati.It is one of the favourite
+        places in the world.
+      </p>
+      <ul>
+        <li>Salem</li>
+        <li>Vellore</li>
+        <li>Tirupati</li>
+      </ul>
+      <ol>
+        <li>Stand in line</li>
+        <li>Be in patti</li>
+        <li>Get the dharshanam</li>
+      </ol>
+    </div>
+  );
+}
+export default MyInfo;
